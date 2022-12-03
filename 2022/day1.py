b0aa2fb678d8fde9cf83fddb24df42ea54eda12b
Ryan Bazzell <rybazzell2002@gmail.com>
2022-12-03 13:40:40 -0500

    security

--- 2022/day1.py
+++ 2022/day1.py
@@ -1,6 +1,6 @@
 from aocd.models import Puzzle
 import os
-os.environ["AOC_SESSION"] = "53616c7465645f5f1428a3a30e1aa3bd9c5faf951111cb16edaa38584883f8c4c0015a743934190eecaf1db7e0bb821b3168d590e18f9090193f56c1a1e14cd2"
+os.environ["AOC_SESSION"] = "53616c7465645f5f1428a3a30e1aa3bd9c5faf951111cb16edaa38584883f8c4c0015a743934190eecaf1db7e0bb821b3168d590e18f9090193f56c1a1e14cd2" # handout: exclude
 
 puzzle = Puzzle(year=2022, day=1)
 #data = puzzle.example_data
