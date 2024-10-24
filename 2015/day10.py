062ecb7084db58aac0b5d31aa55f4f63572b4806
Ryan Bazzell <rybazzell2002@gmail.com>
2024-10-24 09:07:53 -0400

    day10year2015

--- 2015/day10.py
+++ 2015/day10.py
@@ -6,19 +6,36 @@ puzzle = aocu.get_puzzle(__file__)
 examples = puzzle.examples #returns list of Example objects
 input_data = puzzle.input_data #returns string of input data
 
-data = examples[0].input_data
-#data = input_data
+data = examples[1].input_data
+data = input_data
 
-data = data.split("\n")
-
+def look_and_say(n:str):
+    result = ""
+    curr = n[0]
+    count = 1
+    for i in range(1, len(n)):
+        if n[i] != curr:
+            result += f"{count}{curr}"
+            count = 1
+            curr = n[i]
+        else:
+            count += 1
+    result += f"{count}{curr}"
+    return result
 
 def solution_a():
-    pass
+    input = data
+    for i in range(40):
+        input = look_and_say(input)
+    return len(input)
 
 def solution_b():
-    pass
+    input = data
+    for i in range(50):
+        input = look_and_say(input)
+    return len(input)
 
-print(solution_a())
+print(solution_b())
 #puzzle.answer_a = solution_a()
-#puzzle.answer_b = solution_b()
+puzzle.answer_b = solution_b()
 
