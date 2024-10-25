1586b34348a2b90edc1763cfb7a73f50f422b972
Ryan Bazzell <rybazzell2002@gmail.com>
2024-10-25 10:06:17 -0400

    day17year2015

--- 2015/day17.py
+++ 2015/day17.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 sys.path.insert(0, '')
 from misc import aoc_utilities as aocu
 
@@ -7,18 +8,33 @@ examples = puzzle.examples #returns list of Example objects
 input_data = puzzle.input_data #returns string of input data
 
 data = examples[0].input_data
-#data = input_data
+data = input_data
 
-data = data.split("\n")
+data = [int(x) for x in data.split("\n")]
 
+LITERS = 150
 
 def solution_a():
-    pass
+    solutions = []
+    for l in range(1, len(data) + 1):
+        for subset in itertools.combinations(data, l):
+            if sum(subset) == LITERS:
+                solutions.append(subset)
+    return len(solutions)
+
 
 def solution_b():
-    pass
+    solutions = []
+    for l in range(1, len(data) + 1):
+        for subset in itertools.combinations(data, l):
+            if sum(subset) == LITERS:
+                solutions.append(subset)
+        if len(solutions) > 0:
+            break
+    return len(solutions)
 
-print(solution_a())
+
+print(solution_b())
 #puzzle.answer_a = solution_a()
 #puzzle.answer_b = solution_b()
 
