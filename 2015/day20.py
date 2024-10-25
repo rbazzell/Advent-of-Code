a4da1c8c2135aeef2505e3a8fd01895afbaa9854
Ryan Bazzell <rybazzell2002@gmail.com>
2024-10-25 16:17:53 -0400

    day20year2015

--- 2015/day20.py
+++ 2015/day20.py
@@ -1,4 +1,5 @@
 import sys
+from sympy import divisors
 sys.path.insert(0, '')
 from misc import aoc_utilities as aocu
 
@@ -6,19 +7,30 @@ puzzle = aocu.get_puzzle(__file__)
 examples = puzzle.examples #returns list of Example objects
 input_data = puzzle.input_data #returns string of input data
 
-data = examples[0].input_data
-#data = input_data
+#data = examples[0].input_data
+data = input_data
 
-data = data.split("\n")
+TARGET = int(data)
 
 
 def solution_a():
-    pass
+    for i in range(1, TARGET + 1):
+        if sum(divisors(i)) * 10 >= TARGET:
+            return i 
+
+def remove_non_fifty(L: list[int], n: int):
+    R = []
+    for x in L:
+        if x * 50 >= n:
+            R.append(x)
+    return R
 
 def solution_b():
-    pass
+    for i in range(1, TARGET + 1):
+        if sum(remove_non_fifty(divisors(i), i)) * 11 >= TARGET:
+            return i 
 
-print(solution_a())
+print(solution_b())
 #puzzle.answer_a = solution_a()
 #puzzle.answer_b = solution_b()
 
