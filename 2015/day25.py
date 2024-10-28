bfbb6ffe78048cb05d4a0d2c82b3829a8642e1f5
Ryan Bazzell <rybazzell2002@gmail.com>
2024-10-28 13:57:29 -0400

    day25year2015

--- 2015/day25.py
+++ 2015/day25.py
@@ -7,13 +7,20 @@ examples = puzzle.examples #returns list of Example objects
 input_data = puzzle.input_data #returns string of input data
 
 data = examples[0].input_data
-#data = input_data
+data = "row: 5, column 6."
+data = input_data
 
-data = data.split("\n")
 
+row = int(data.split(" ")[-3][:-1])
+col = int(data.split(" ")[-1][:-1])
 
 def solution_a():
-    pass
+    n = (row + col - 1)
+    iters = (n * (n + 1) // 2) - row
+    code = 20151125
+    for iter in range(iters):
+        code = (code * 252533) % 33554393
+    return code
 
 def solution_b():
     pass
