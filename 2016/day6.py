fdcf00f4ef3a0aabcf3b763fb85f086e8825044e
Ryan Bazzell <rybazzell2002@gmail.com>
2024-10-29 11:45:28 -0400

    day6year2016

--- 2016/day6.py
+++ 2016/day6.py
@@ -7,18 +7,27 @@ examples = puzzle.examples #returns list of Example objects
 input_data = puzzle.input_data #returns string of input data
 
 data = examples[0].input_data
-#data = input_data
+data = input_data
 
 data = data.split("\n")
-
+position = ["" for i in range(len(data[0]))]
+for i in range(len(data[0])):
+    for j in range(len(data)):
+        position[i] += data[j][i]
 
 def solution_a():
-    pass
+    output = ""
+    for column in position:
+        output += max(column, key=column.count)
+    return output
 
 def solution_b():
-    pass
+    output = ""
+    for column in position:
+        output += min(column, key=column.count)
+    return output
 
-print(solution_a())
+print(solution_b())
 #puzzle.answer_a = solution_a()
 #puzzle.answer_b = solution_b()
 
