b0570449f8175c8482cb0ff964fc5afe5bd36dfb
Ryan Bazzell <rybazzell2002@gmail.com>
2024-11-06 16:08:46 -0500

    day15year2016

--- 2016/day15.py
+++ 2016/day15.py
@@ -7,18 +7,51 @@ examples = puzzle.examples #returns list of Example objects
 input_data = puzzle.input_data #returns string of input data
 
 data = examples[0].input_data
-#data = input_data
+data = input_data
 
 data = data.split("\n")
 
+discs = {}
+for i in range(len(data)):
+    line = data[i].split(" ")
+    positions = int(line[3])
+    start_position = int(line[-1][:-1])
+    discs[i + 1] = (positions, start_position)
+
+def product(iterable):
+    product = 1
+    for item in iterable:
+        product *= item
+    return product
+
 
 def solution_a():
-    pass
+    for t in range(product(info[0] for info in discs.values())):
+        for d, info in discs.items():
+            p, s = info
+            found = True
+            if (t + d + s) % p != 0:
+                found = False
+                break
+        if found:
+            return t
+    return -1
+
 
 def solution_b():
-    pass
+    discs[i + 2] = (11, 0)
+    for t in range(product(info[0] for info in discs.values())):
+        for d, info in discs.items():
+            p, s = info
+            found = True
+            if (t + d + s) % p != 0:
+                found = False
+                break
+        if found:
+            return t
+    return -1
 
-print(solution_a())
+print(solution_b())
 #puzzle.answer_a = solution_a()
 #puzzle.answer_b = solution_b()
 
