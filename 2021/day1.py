8bb5c7ba40d1dc43269d9ca8cc9cd52a604c72f0
Ryan Bazzell <rbbazzell7@gmail.com>
2024-12-11 08:18:12 -0500

    day1year2021

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,19 +1,32 @@
-from aocd.models import Puzzle
+import sys
+sys.path.insert(0, '')
+from misc import aoc_utilities as aocu
 
-puzzle = Puzzle(year=2021, day=1)
-data = [int(x) for x in puzzle.input_data.split("\n")]
+puzzle = aocu.get_puzzle(__file__)
+examples = puzzle.examples #returns list of Example objects
+input_data = puzzle.input_data #returns string of input data
 
-increased = 0
-for i in range(1, len(data)):
-    if data[i] > data[i - 1]:
-        increased += 1
-puzzle.answer_a = increased
-print(f"Part 1: {increased}")
+data = examples[0].input_data
+data = input_data
 
-increased = 0
-for i in range(3, len(data)):
-    if data[i] + data[i - 1] + data[i - 2] > data[i - 1] + data[i - 2] + data[i - 3]:
-        increased += 1
-puzzle.answer_b = increased
-print(f"Part 2: {increased}")
+sonar = [int(x) for x in data.split("\n")]
+
+def solution_a():
+    increases = 0
+    for i in range(1, len(sonar)):
+        if sonar[i] > sonar[i - 1]:
+            increases += 1
+    return increases
+
+
+def solution_b():
+    increases = 0
+    for i in range(3, len(sonar)):
+        if sum(sonar[i-2:i+1]) > sum(sonar[i-3:i]):
+            increases += 1
+    return increases
+
+print(solution_b())
+#puzzle.answer_a = solution_a()
+#puzzle.answer_b = solution_b()
 
